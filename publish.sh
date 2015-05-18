bd56205f6ef6f679cea9bb2b074ca9251775d5b9
Xiaodong Wang <xiaodongw79@gmail.com>
2015-05-17 22:08:21 -0700

    Add publish.sh to publish all artifacts.

---
+++ publish.sh
@@ -0,0 +1,3 @@
+gradle clean uploadArchives -PscalaVersion=2.10.4 -PfinatraVersion=1.6.0
+gradle clean uploadArchives -PscalaVersion=2.10.4 -PfinatraVersion=2.0.0.M1
+gradle clean uploadArchives -PscalaVersion=2.11.6 -PfinatraVersion=2.0.0.M1
