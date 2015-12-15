c68ccc6d4385ec439d58749ea8bf2914e0ef9528
Xiaodong Wang <xiaodongw79@gmail.com>
2015-12-14 20:40:01 -0800

    Update the sample to demo usage of ApiModelProperty.

--- src/test/scala/com/github/xiaodongw/swagger/finatra/Models.scala
+++ src/test/scala/com/github/xiaodongw/swagger/finatra/Models.scala
@@ -8,14 +8,17 @@ case class Address(street: String, zip: String)
 
 case class Student(firstName: String, lastName: String, gender: Gender, birthday: LocalDate, grade: Int, address: Option[Address])
 
-//todo scala enumeration is not supported
 object CourseType extends Enumeration {
   val LEC, LAB = Value
 }
 
 case class Course(time: DateTime,
                   name: String,
+                  @ApiModelProperty(required = false, example = "[math,stem]")
                   tags: Seq[String],
-                  @ApiModelProperty(dataType = "string", allowableValues = "LEC,LAB") typ: CourseType.Value,
+                  @ApiModelProperty(dataType = "string", allowableValues = "LEC,LAB")
+                  typ: CourseType.Value,
+                  @ApiModelProperty(readOnly = true)
                   capacity: Int,
-                  @ApiModelProperty(dataType = "double") cost: BigDecimal)
+                  @ApiModelProperty(dataType = "double", required = true)
+                  cost: BigDecimal)
