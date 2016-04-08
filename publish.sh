ec0f37fbafd10c4857e6ebe97ada84b54e345682
Xiaodong Wang <xiaodongw79@gmail.com>
2016-04-07 20:38:13 -0700

    Upgrade Finatra to 2.1.5. Publish 0.5.1.

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 gradle clean uploadArchives -PscalaVersion=2.10.6 -PfinatraVersion=1.6.0
-gradle clean uploadArchives -PscalaVersion=2.10.6 -PfinatraVersion=2.1.3
-gradle clean uploadArchives -PscalaVersion=2.11.7 -PfinatraVersion=2.1.3
+gradle clean uploadArchives -PscalaVersion=2.10.6 -PfinatraVersion=2.1.5
+gradle clean uploadArchives -PscalaVersion=2.11.7 -PfinatraVersion=2.1.5
